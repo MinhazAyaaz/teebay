552ce108965aac807693faed112f4291ac369f73
Minhaz <minhazabedin1@gmail.com>
2025-10-07 17:21:38 +0600

    feat: added @mantine/notifications dependency and integrate notification styles in main rendering structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
 import './index.css'
+import { Notifications } from '@mantine/notifications';
 import App from './App.tsx'
 import { MantineProvider } from '@mantine/core'
 import { AuthProvider } from './context/AuthProvider.tsx'
@@ -12,6 +14,7 @@ import { apolloClient } from './graphql/client'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
+      <Notifications position='top-right'/>
       <ApolloProvider client={apolloClient}>
         <AuthProvider>
           <App />
